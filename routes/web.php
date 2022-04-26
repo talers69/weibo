25e9e52d3667a9ee3ccba856d8ea053a20de3132
talers69 <talers69@163.com>
2022-04-26 16:03:14 +0800

    基础页面

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/* Route::get('/', function () {
+    //return view('welcome');
+    return "hello world!";
+}); */
+
+
+Route::get('/', 'StaticPagesController@home');
+Route::get('/help', 'StaticPagesController@help');
+Route::get('/about', 'StaticPagesController@about');
