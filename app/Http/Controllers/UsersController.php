332761b9a46b225d0a1a406db6f7a58ac053d3ab
talers69 <talers69@163.com>
2022-04-27 15:51:05 +0800

    移除邮件发送的 from 选项

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -95,8 +95,8 @@ class UsersController extends Controller
     {
         $view = 'emails.confirm';
         $data = compact('user');
-        $from = 'summer@example.com';
-        $name = 'Summer';
+        /* $from = 'summer@example.com';
+        $name = 'Summer'; */
         $to = $user->email;
         $subject = "感谢注册 Weibo 应用！请确认你的邮箱。";
 
