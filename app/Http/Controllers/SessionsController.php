69701ccf06f6e3f98c9d3d504337338bb3d8abfa
talers69 <talers69@163.com>
2022-04-27 11:13:48 +0800

    登录时记住我

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -22,7 +22,7 @@ class SessionsController extends Controller
             // 该用户存在于数据库，且邮箱和密码相符合
         } */
 
-        if (Auth::attempt($credentials)) {
+        if (Auth::attempt($credentials, $request->has('remember'))) {
             // 登录成功后的相关操作
             session()->flash('success', '欢迎回来！');
             return redirect()->route('users.show', [Auth::user()]);
